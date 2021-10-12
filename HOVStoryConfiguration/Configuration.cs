fa8d591737475955ed4d2ab1f426f74e980ee228
Tran Phong <n.tranphongse@gmail.com>
2021-10-12 22:15:58 +0700

    Create Configuration Project
    Get Database Configurations

---
+++ HOVStoryConfiguration/Configuration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOVStoryConfiguration
+{
+    public static class Configuration
+    {
+        /// <summary>
+        /// Connection string for MongoDB at HOVStory
+        /// </summary>
+        public static string ConnectionString
+        {
+            get => ConfigurationDAO.Instance.GetConfiguration().GetSection("HOVStoryDB")["ConnectionString"];
+        }
+
+        /// <summary>
+        /// Default Database name for the project
+        /// </summary>
+        public static string DatabaseName
+        {
+            get => ConfigurationDAO.Instance.GetConfiguration().GetSection("HOVStoryDB")["DatabaseName"];
+        }
+
+        /// <summary>
+        /// Confessions Table Name in the Database
+        /// </summary>
+        public static string ConfessionsTableName
+        {
+            get => ConfigurationDAO.Instance.GetConfiguration().GetSection("HOVStoryDB")["ConfessionsTable"];
+        }
+
+        /// <summary>
+        /// Users Table name in the Database
+        /// </summary>
+        public static string UsersTableName
+        {
+            get => ConfigurationDAO.Instance.GetConfiguration().GetSection("HOVStoryDB")["UsersTable"];
+        }
+    }
+}
---
+++ HOVStoryConfiguration/ConfigurationDAO.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOVStoryConfiguration
+{
+    internal class ConfigurationDAO
+    {
+        private static ConfigurationDAO instance = null;
+        private static readonly object instanceLock = new object();
+        private static IConfigurationRoot Configuration = null;
+
+        private ConfigurationDAO()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true);
+
+            Configuration = builder.Build();
+        }
+
+        internal static ConfigurationDAO Instance
+        {
+            get
+            {
+                lock(instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new ConfigurationDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        internal IConfigurationRoot GetConfiguration() => Configuration;
+    }
+}
