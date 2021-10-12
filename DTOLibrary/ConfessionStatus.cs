a7c511d5dbd6863adb479b720c0bad2e983a15bb
Tran Phong <n.tranphongse@gmail.com>
2021-10-12 22:14:53 +0700

    Add ConfessionDTO

---
+++ DTOLibrary/ConfessionStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DTOLibrary
+{
+    public static class ConfessionStatus
+    {
+        public static readonly string Approved = "A";
+        public static readonly string Rejected = "R";
+        public static readonly string Pending = "P";
+
+        public static readonly Regex CheckStatus = new Regex(@"^[" + Approved + Rejected + Pending + @"]$");
+    }
+}
---
+++ DTOLibrary/Confessions.cs
@@ -0,0 +1,65 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOLibrary
+{
+    public class Confessions
+    {
+        /// <summary>
+        /// The Object ID matched with the one in MongoDB
+        /// </summary>
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Content of the Confession
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// Image URL of the Confession, it should be uploaded to Firebase or somewhere else and get the URL
+        /// </summary>
+        public string Image { get; set; }
+
+        /// <summary>
+        /// The time that the confession was sent
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// Status of the Confession
+        /// <seealso cref="ConfessionStatus"/>
+        /// </summary>
+        public string Status { 
+            get => Status; 
+            set
+            {
+                if (!ConfessionStatus.CheckStatus.IsMatch(value)) {
+                    throw new Exception("Confession's Status is invalid!");
+                }
+                Status = value;
+            }
+        }
+
+        /// <summary>
+        /// Admin name that approved or rejected the confession
+        /// </summary>
+        public string Admin { get; set; }
+
+        /// <summary>
+        /// Note of the admin when rejecting the confession
+        /// </summary>
+        public string Comment { get; set; }
+
+        /// <summary>
+        /// The time that the confession was modified (approved or rejected) by the admin. It is the same as the CreatedAt when it is pending for approval
+        /// </summary>
+        public DateTime ModifiedOn { get; set; }
+    }
+}
