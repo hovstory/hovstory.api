b97a6ef3103b05cfe4e0495f58c3e19361e32c42
Tran Phong <n.tranphongse@gmail.com>
2021-10-13 16:30:13 +0700

    Complete for Configuration Project

--- HOVStoryConfiguration/Configuration.cs
+++ HOVStoryConfiguration/Configuration.cs
@@ -10,36 +10,57 @@ namespace HOVStoryConfiguration
 {
     public static class Configuration
     {
+        #region Private Members to get Configuration
+        private static IConfigurationRoot GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true);
+
+            return builder.Build();
+        }
+        #endregion
+
+        #region Public Configuration Fields
+
+        #region Database Stuffs
         /// <summary>
         /// Connection string for MongoDB at HOVStory
         /// </summary>
-        public static string ConnectionString
-        {
-            get => ConfigurationDAO.Instance.GetConfiguration().GetSection("HOVStoryDB")["ConnectionString"];
-        }
+        public static string ConnectionString => GetConfiguration().GetSection("HOVStoryDB")["ConnectionString"];
 
         /// <summary>
         /// Default Database name for the project
         /// </summary>
-        public static string DatabaseName
-        {
-            get => ConfigurationDAO.Instance.GetConfiguration().GetSection("HOVStoryDB")["DatabaseName"];
-        }
+        public static string DatabaseName => GetConfiguration().GetSection("HOVStoryDB")["DatabaseName"];
 
         /// <summary>
         /// Confessions Table Name in the Database
         /// </summary>
-        public static string ConfessionsTableName
-        {
-            get => ConfigurationDAO.Instance.GetConfiguration().GetSection("HOVStoryDB")["ConfessionsTable"];
-        }
+        public static string ConfessionsTableName => GetConfiguration().GetSection("HOVStoryDB")["ConfessionsTable"];
 
         /// <summary>
         /// Users Table name in the Database
         /// </summary>
-        public static string UsersTableName
-        {
-            get => ConfigurationDAO.Instance.GetConfiguration().GetSection("HOVStoryDB")["UsersTable"];
-        }
+        public static string UsersTableName => GetConfiguration().GetSection("HOVStoryDB")["UsersTable"];
+        #endregion
+        #region Confession Status
+        /// <summary>
+        /// Approved Status for confession
+        /// </summary>
+        public static string ConfessionApproved => GetConfiguration().GetSection("ConfessionStatus")["Approved"];
+
+        /// <summary>
+        /// Rejected Status for confession
+        /// </summary>
+        public static string ConfessionRejected => GetConfiguration().GetSection("ConfessionStatus")["Rejected"];
+
+        /// <summary>
+        /// Pening Status for confession
+        /// </summary>
+        public static string ConfessionPending => GetConfiguration().GetSection("ConfessionStatus")["Pending"];
+        #endregion
+
+        #endregion
     }
 }
