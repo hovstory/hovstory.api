e7517d945c3d6110338653b0c68eaa95aebefc2e
Tran Phong <n.tranphongse@gmail.com>
2021-10-13 16:31:52 +0700

    Create Test for Configuration

---
+++ DTOTest/TestConfiguration.cs
@@ -0,0 +1,58 @@
+﻿using HOVStoryConfiguration;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOTest
+{
+    [TestFixture]
+    public class TestConfiguration
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void TestDatabaseName()
+        {
+            Assert.AreEqual("HOVStory", Configuration.DatabaseName);
+        }
+
+        [Test]
+        public void TestConnectionString()
+        {
+            Assert.AreEqual("mongodb+srv://hovstory:HOVStory@humansofvothisaudb.uevma.mongodb.net/HOVStory?retryWrites=true&w=majority", Configuration.ConnectionString);
+        }
+        [Test]
+        public void TestConfessionsName()
+        {
+            Assert.AreEqual("Confessions", Configuration.ConfessionsTableName);
+        }
+        [Test]
+        public void TestUsersName()
+        {
+            Assert.AreEqual("Users", Configuration.UsersTableName);
+        }
+        [Test]
+        public void TestApproved()
+        {
+            Assert.AreEqual("A", Configuration.ConfessionApproved);
+        }
+        [Test]
+        public void TestRejected()
+        {
+            Assert.AreEqual("R", Configuration.ConfessionRejected);
+        }
+        [Test]
+        public void TestPending()
+        {
+            Assert.AreEqual("P", Configuration.ConfessionPending);
+        }
+
+    }
+}
