9e0ded5701ac822533543ccf319915c3cb86110f
Tran Phong <n.tranphongse@gmail.com>
2021-10-13 16:32:33 +0700

    Create HovStoryContext - context to connect to database

---
+++ DTOLibrary/HovStoryContext.cs
@@ -0,0 +1,36 @@
+﻿using HOVStoryConfiguration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOLibrary
+{
+    /// <summary>
+    /// Database Context class for accessing collections in MongoDB
+    /// </summary>
+    public class HovStoryContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public HovStoryContext()
+        {
+            var client = new MongoClient(Configuration.ConnectionString);
+            _database = client.GetDatabase(Configuration.DatabaseName);
+        }
+
+        /// <summary>
+        /// The Collection for Confessions
+        /// </summary>
+        public IMongoCollection<Confessions> Confessions 
+            => _database.GetCollection<Confessions>(Configuration.ConfessionsTableName);
+
+        /// <summary>
+        /// The Collection for Users
+        /// </summary>
+        public IMongoCollection<User> Users
+            => _database.GetCollection<User>(Configuration.UsersTableName);
+    }
+}
