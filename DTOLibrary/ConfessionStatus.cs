3d0cda96fa08c29ceb1aa8528856d6214946d59c
Tran Phong <n.tranphongse@gmail.com>
2021-10-13 16:32:11 +0700

    Update ConfessionStatus to use Configuration

--- DTOLibrary/ConfessionStatus.cs
+++ DTOLibrary/ConfessionStatus.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HOVStoryConfiguration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,9 @@ namespace DTOLibrary
 {
     public static class ConfessionStatus
     {
-        public static readonly string Approved = "A";
-        public static readonly string Rejected = "R";
-        public static readonly string Pending = "P";
+        public static readonly string Approved = Configuration.ConfessionApproved;
+        public static readonly string Rejected = Configuration.ConfessionRejected;
+        public static readonly string Pending = Configuration.ConfessionPending;
 
         public static readonly Regex CheckStatus = new Regex(@"^[" + Approved + Rejected + Pending + @"]$");
     }
