c128646849b1d0ac69105510072646ce8427e271
Tran Phong <n.tranphongse@gmail.com>
2021-10-13 17:34:27 +0700

    Create Test for ConfessionDAO

---
+++ DTOTest/TestConfessionDAO.cs
@@ -0,0 +1,29 @@
+﻿using DAOLibrary.Repository;
+using DTOLibrary;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOTest
+{
+    [TestFixture]
+    public class TestConfessionDAO
+    {
+        private IConfessionRepository confessionRepository;
+        [SetUp]
+        public void SetUp()
+        {
+            confessionRepository = new ConfessionRepository();
+        }
+
+        [Test]
+        public void TestGetConfessions()
+        {
+            var confessions = confessionRepository.GetConfessions();
+            Assert.AreEqual(0, confessions.Count);
+        }
+    }
+}
