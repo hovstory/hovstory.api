28268f7c193d0f9f301f74bf5665961dd8745b32
Tran Phong <n.tranphongse@gmail.com>
2021-10-14 17:25:28 +0700

    Test for UserDAO

---
+++ DTOTest/TestUserDAO.cs
@@ -0,0 +1,28 @@
+﻿using DAOLibrary.Repository;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOTest
+{
+    [TestFixture]
+    public class TestUserDAO
+    {
+        private IUserRepository userRepository;
+        [SetUp]
+        public void SetUp()
+        {
+            userRepository = new UserRepository();
+        }
+
+        [Test]
+        public void TestGetUser()
+        {
+            var user = userRepository.Get("n.tranphongse@gmail.com");
+            Assert.IsNull(user);
+        }
+    }
+}
