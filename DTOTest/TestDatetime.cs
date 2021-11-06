33876413523777197a8790bf6441f7f9d2492a83
Tran Phong <n.tranphongse@gmail.com>
2021-11-06 21:14:43 +0700

    Test for Utils

---
+++ DTOTest/TestDatetime.cs
@@ -0,0 +1,28 @@
+﻿using HOVStoryUtils;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOTest
+{
+    [TestFixture]
+    class TestDatetime
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void ConvertDatetime()
+        {
+            DateTime dateTime = new DateTime(2021, 11, 06, 10, 15, 25);
+            DateTime vnTime = Utils.Datetime.ConvertToVn(dateTime);
+            Assert.AreEqual(vnTime, new DateTime(2021, 11, 06, 17, 15, 25));
+        }
+    }
+}
